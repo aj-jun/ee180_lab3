647526ca5826f84e3c9355e7eb0036d7f30e3914
Ashley Soojin Jun <ajjun@rice-03.farmshare.sunet>
2026-02-25 13:07:39 -0800

    generating bit stream

---
+++ lab3/sim/tests/sobel_c/build/sobel_mips.c.d
@@ -0,0 +1,64 @@
+build/./sobel_mips.c.o: sobel_mips.c \
+ /afs/ir.stanford.edu/class/ee180/labs/lab3/mips_cc/mips_tc/mips-elf/include/stdlib.h \
+ /afs/ir.stanford.edu/class/ee180/labs/lab3/mips_cc/mips_tc/mips-elf/include/machine/ieeefp.h \
+ /afs/ir.stanford.edu/class/ee180/labs/lab3/mips_cc/mips_tc/mips-elf/include/_ansi.h \
+ /afs/ir.stanford.edu/class/ee180/labs/lab3/mips_cc/mips_tc/mips-elf/include/newlib.h \
+ /afs/ir.stanford.edu/class/ee180/labs/lab3/mips_cc/mips_tc/mips-elf/include/_newlib_version.h \
+ /afs/ir.stanford.edu/class/ee180/labs/lab3/mips_cc/mips_tc/mips-elf/include/sys/config.h \
+ /afs/ir.stanford.edu/class/ee180/labs/lab3/mips_cc/mips_tc/mips-elf/include/sys/features.h \
+ /afs/ir.stanford.edu/class/ee180/labs/lab3/mips_cc/mips_tc/lib/gcc/mips-elf/6.3.0/include/stddef.h \
+ /afs/ir.stanford.edu/class/ee180/labs/lab3/mips_cc/mips_tc/mips-elf/include/sys/reent.h \
+ /afs/ir.stanford.edu/class/ee180/labs/lab3/mips_cc/mips_tc/mips-elf/include/_ansi.h \
+ /afs/ir.stanford.edu/class/ee180/labs/lab3/mips_cc/mips_tc/mips-elf/include/sys/_types.h \
+ /afs/ir.stanford.edu/class/ee180/labs/lab3/mips_cc/mips_tc/mips-elf/include/machine/_types.h \
+ /afs/ir.stanford.edu/class/ee180/labs/lab3/mips_cc/mips_tc/mips-elf/include/machine/_default_types.h \
+ /afs/ir.stanford.edu/class/ee180/labs/lab3/mips_cc/mips_tc/mips-elf/include/sys/lock.h \
+ /afs/ir.stanford.edu/class/ee180/labs/lab3/mips_cc/mips_tc/mips-elf/include/sys/cdefs.h \
+ /afs/ir.stanford.edu/class/ee180/labs/lab3/mips_cc/mips_tc/mips-elf/include/machine/stdlib.h \
+ /afs/ir.stanford.edu/class/ee180/labs/lab3/mips_cc/mips_tc/mips-elf/include/alloca.h \
+ /afs/ir.stanford.edu/class/ee180/labs/lab3/mips_cc/mips_tc/lib/gcc/mips-elf/6.3.0/include/stdint.h \
+ /afs/ir.stanford.edu/class/ee180/labs/lab3/mips_cc/mips_tc/mips-elf/include/stdint.h \
+ /afs/ir.stanford.edu/class/ee180/labs/lab3/mips_cc/mips_tc/mips-elf/include/sys/_intsup.h \
+ /afs/ir.stanford.edu/class/ee180/labs/lab3/mips_cc/mips_tc/mips-elf/include/sys/_stdint.h
+
+/afs/ir.stanford.edu/class/ee180/labs/lab3/mips_cc/mips_tc/mips-elf/include/stdlib.h:
+
+/afs/ir.stanford.edu/class/ee180/labs/lab3/mips_cc/mips_tc/mips-elf/include/machine/ieeefp.h:
+
+/afs/ir.stanford.edu/class/ee180/labs/lab3/mips_cc/mips_tc/mips-elf/include/_ansi.h:
+
+/afs/ir.stanford.edu/class/ee180/labs/lab3/mips_cc/mips_tc/mips-elf/include/newlib.h:
+
+/afs/ir.stanford.edu/class/ee180/labs/lab3/mips_cc/mips_tc/mips-elf/include/_newlib_version.h:
+
+/afs/ir.stanford.edu/class/ee180/labs/lab3/mips_cc/mips_tc/mips-elf/include/sys/config.h:
+
+/afs/ir.stanford.edu/class/ee180/labs/lab3/mips_cc/mips_tc/mips-elf/include/sys/features.h:
+
+/afs/ir.stanford.edu/class/ee180/labs/lab3/mips_cc/mips_tc/lib/gcc/mips-elf/6.3.0/include/stddef.h:
+
+/afs/ir.stanford.edu/class/ee180/labs/lab3/mips_cc/mips_tc/mips-elf/include/sys/reent.h:
+
+/afs/ir.stanford.edu/class/ee180/labs/lab3/mips_cc/mips_tc/mips-elf/include/_ansi.h:
+
+/afs/ir.stanford.edu/class/ee180/labs/lab3/mips_cc/mips_tc/mips-elf/include/sys/_types.h:
+
+/afs/ir.stanford.edu/class/ee180/labs/lab3/mips_cc/mips_tc/mips-elf/include/machine/_types.h:
+
+/afs/ir.stanford.edu/class/ee180/labs/lab3/mips_cc/mips_tc/mips-elf/include/machine/_default_types.h:
+
+/afs/ir.stanford.edu/class/ee180/labs/lab3/mips_cc/mips_tc/mips-elf/include/sys/lock.h:
+
+/afs/ir.stanford.edu/class/ee180/labs/lab3/mips_cc/mips_tc/mips-elf/include/sys/cdefs.h:
+
+/afs/ir.stanford.edu/class/ee180/labs/lab3/mips_cc/mips_tc/mips-elf/include/machine/stdlib.h:
+
+/afs/ir.stanford.edu/class/ee180/labs/lab3/mips_cc/mips_tc/mips-elf/include/alloca.h:
+
+/afs/ir.stanford.edu/class/ee180/labs/lab3/mips_cc/mips_tc/lib/gcc/mips-elf/6.3.0/include/stdint.h:
+
+/afs/ir.stanford.edu/class/ee180/labs/lab3/mips_cc/mips_tc/mips-elf/include/stdint.h:
+
+/afs/ir.stanford.edu/class/ee180/labs/lab3/mips_cc/mips_tc/mips-elf/include/sys/_intsup.h:
+
+/afs/ir.stanford.edu/class/ee180/labs/lab3/mips_cc/mips_tc/mips-elf/include/sys/_stdint.h:
